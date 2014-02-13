17193a4a81e698f2d5117679c458d4d535e29fa0
renansep <renan@Renans-MacBook-Pro-2.local>
2014-02-13 11:21:52 -0200

    Main screen

---
+++ LOO2/LOO2/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  LOO2
+//
+//  Created by Renan Cargnin on 2/13/14.
+//  Copyright (c) 2014 Vinicius Soares Lima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ LOO2/LOO2/MainViewController.m
@@ -0,0 +1,44 @@
+//
+//  MainViewController.m
+//  LOO2
+//
+//  Created by Renan Cargnin on 2/13/14.
+//  Copyright (c) 2014 Vinicius Soares Lima. All rights reserved.
+//
+
+#import "MainViewController.h"
+#import "CharactersViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)showCharacterView:(UIButton *)sender {
+    CharactersViewController *controller = [self.storyboard instantiateViewControllerWithIdentifier:@"characters"];
+    [self presentViewController:controller animated:YES completion:nil];
+}
+
+@end
