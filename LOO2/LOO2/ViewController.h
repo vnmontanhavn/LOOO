987b1cfc4035e275f84b1520f267f42e7a98c1b1
Vinicius Soares Lima <vnmontanhavn@MacBook-Pro-de-Vinicius.local>
2014-02-13 09:08:51 -0200

    Iniciando

---
+++ LOO2/LOO2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LOO2
+//
+//  Created by Vinicius Soares Lima on 13/02/14.
+//  Copyright (c) 2014 Vinicius Soares Lima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ LOO2/LOO2/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LOO2
+//
+//  Created by Vinicius Soares Lima on 13/02/14.
+//  Copyright (c) 2014 Vinicius Soares Lima. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
